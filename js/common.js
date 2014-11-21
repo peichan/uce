df29b9be9509e12de3d341a0d288ee798a3bb5e1
peichan <ktgchyshnr13@gmail.com>
2014-11-21 15:28:17 +0900

    ヘッダー読み込みをすべてのhtmlに適応

--- js/common.js
+++ js/common.js
@@ -1,13 +1,5 @@
 $(function(){
 
-    // $.ajax({
-    //     url: "../header.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準になります
-    //     cache: false, //キャッシュを利用するか（お好みで）
-    //     async: false, //非同期で読み込むか（お好みで）
-    //     success: function(html){
-    //         document.write(html);
-    //     }
-    // });
     var header = $("#header");
     if(location.host == "peichan.github.io"){
         header.load("http://" + location.host + "/uce/header.html");
