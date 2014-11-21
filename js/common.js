16d7c3307c35d27b7708801a89523fab4dbfd2cf
peichan <ktgchyshnr13@gmail.com>
2014-11-21 16:04:38 +0900

    今開いているページのリンクをactiveに

--- js/common.js
+++ js/common.js
@@ -2,8 +2,18 @@ $(function(){
 
     var header = $("#header");
     if(location.host == "peichan.github.io"){
-        header.load("http://" + location.host + "/uce/header.html");
+        header.load("http://" + location.host + "/uce/header.html", function() {
+            activate_link();
+        });
     }else{
-        header.load("./header.html");
+        header.load("./header.html", function() {
+            activate_link();
+        });
     };
 });
+
+// 今開いているページをactiveに
+function activate_link(){
+    var active_html = "." + location.pathname;
+    $('a[href="' + active_html + '"]').parent().addClass("active");
+};
